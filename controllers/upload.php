e22ab529aa56fd826b8025630659427fba52d862
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2023-05-26 15:24:26 -0300

    cadastro quase funcional

---
+++ controllers/upload.php
@@ -0,0 +1,43 @@
+<?php
+
+include("../models/conexao.php");
+
+$diretorio = "imgs/";
+
+
+/* Implementações (pathinfo)(md5()) (uniqid):
+ -  Apenas arquivos png;
+ -  Na hora que fizer o upload vai renomear o arquivo para um nome aleatório, mas no banco de dados fica salvo o nome antigo e o novo nome.
+ */
+
+/* Operador ternário */
+$arquivo = isset($_FILES['arquivo']) ? $_FILES['arquivo'] : FALSE;
+
+for ($k = 0; $k < count($arquivo['name']); $k++) {
+
+	$destino = $diretorio . "/" . $arquivo['name'][$k];
+
+	/* Extensão: */
+	$extension = pathinfo($destino, PATHINFO_EXTENSION);
+
+	$nomeImagem = $arquivo['name'][$k];
+	$nomeImagemRandom = md5(uniqid($nomeImagem));
+	/* ou $varImgNameRandom = uniqid() . "." . $extension;
+	 */
+
+	/* move_uploaded_file(arquivo, destino do arquivo) */
+	if ($extension == "png") {
+		/* Mover arquivo para a pasta com o nome randomico */
+		if (move_uploaded_file($arquivo['tmp_name'][$k], $diretorio . "/" . $varImgNameRandom . "." . $extension)) {
+
+			mysqli_query($conexao, "INSERT INTO imagens (nome_imagem, nome_randomico_imagem) VALUES ('$nomeImagem', '$nomeImagemRandom.$extension')");
+			echo " <br> Arquivo enviado com sucesso! <br>";
+
+		} else {
+			echo "Falha ao enviar o arquivo";
+		}
+	} else {
+		echo "Arquivo não é compatível com o tipo 'PNG'";
+	}
+}
+?>
--- views/cadastro.php
+++ views/cadastro.php
@@ -8,7 +8,25 @@
     <label class="form-label">Conteúdo</label>
     <input class="form-control"type="text" name="blogCorpo"> <br>
     <label class="form-date" >Data</label> <br>
-    <input type="date" name="blogData">
+    <input type="date" name="blogData"> <br>
+    <label for="usuario">Selecione um usuario</label>
+        <select name="cars" id="cars" name="blogUsuario">
+            <?PHP
+            $query =  mysqli_query($conexao, "SELECT count(*) as usuarios from usuario") ;
+            for ($k = 0; $k < $query; $k++) {
+            ?>
+            <?php $query = mysqli_query($conexao, "SELECT * from posts INNER JOIN bloginfo ON blog_bloginfo_codigo = bloginfo_codigo INNER JOIN imagens on blog_blogimgs_codigo = id_imagem INNER JOIN usuario ON blog_usuario_codigo = usuario_codigo group by blog_bloginfo_codigo;")?>
+            <option value="usuario<?php echo $exibe[11]?>"><?PHP echo $exibe[12] ?></option>
+            <?php }?>
+        </select>
+            <br>
+     <!-- Se tirar enctype ele para de funcionar, ele mostra o tipo de form -->
+     <form name="upload" enctype="multipart/form-data" method="post" action="../controllers/upload.php">
+        <input type="hidden" name="MAX_FILE_SIZE" value="99999999">
+        <input type="file" name="arquivo[]" multiple="multiple"/>
+        <input name="enviar" type="submit" value="Enviar">
+    </form>
+
     <input class="btn btn-success mt-3 ms-1" type="submit" value="Cadastrar">
 
 </form>
