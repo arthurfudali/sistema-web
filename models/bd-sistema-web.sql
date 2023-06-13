2306bf33fc91d6aedbd996ba718e58dccbcbbc0a
Arthur Ribeiro Dias Fudali <arthurfudali5@gmail.com>
2023-06-13 20:49:43 -0300

    novo bd

--- models/bd-sistema-web.sql
+++ models/bd-sistema-web.sql
@@ -1,36 +1,37 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           10.1.33-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win32
--- HeidiSQL Versão:              9.5.0.5196
+-- Versão do servidor:           10.4.27-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.3.0.6589
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Copiando estrutura do banco de dados para uploadarquivos
-CREATE DATABASE IF NOT EXISTS `uploadarquivos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `uploadarquivos` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
 USE `uploadarquivos`;
 
 -- Copiando estrutura para tabela uploadarquivos.bloginfo
 CREATE TABLE IF NOT EXISTS `bloginfo` (
   `bloginfo_codigo` int(11) NOT NULL AUTO_INCREMENT,
   `bloginfo_titulo` varchar(250) DEFAULT NULL,
-  `bloginfo_corpo` longtext,
+  `bloginfo_corpo` longtext DEFAULT NULL,
   `bloginfo_data` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`bloginfo_codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Copiando dados para a tabela uploadarquivos.bloginfo: ~2 rows (aproximadamente)
+-- Copiando dados para a tabela uploadarquivos.bloginfo: ~1 rows (aproximadamente)
 DELETE FROM `bloginfo`;
-/*!40000 ALTER TABLE `bloginfo` DISABLE KEYS */;
 INSERT INTO `bloginfo` (`bloginfo_codigo`, `bloginfo_titulo`, `bloginfo_corpo`, `bloginfo_data`) VALUES
-	(2, 'igor 2 ', 'igor é igor', '2023-05-25 00:00:00');
-/*!40000 ALTER TABLE `bloginfo` ENABLE KEYS */;
+	(30, 'Finalizado! ', 'acabaram os requisitos do form!! (acho)', '2023-06-13 03:00:00');
 
 -- Copiando estrutura para tabela uploadarquivos.imagens
 CREATE TABLE IF NOT EXISTS `imagens` (
@@ -38,24 +39,19 @@ CREATE TABLE IF NOT EXISTS `imagens` (
   `nome_imagem` varchar(100) DEFAULT NULL,
   `nome_randomico_imagem` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_imagem`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Copiando dados para a tabela uploadarquivos.imagens: ~2 rows (aproximadamente)
+-- Copiando dados para a tabela uploadarquivos.imagens: ~1 rows (aproximadamente)
 DELETE FROM `imagens`;
-/*!40000 ALTER TABLE `imagens` DISABLE KEYS */;
 INSERT INTO `imagens` (`id_imagem`, `nome_imagem`, `nome_randomico_imagem`) VALUES
-	(1, 'foto.png', '7d0f79df27ae30c96f54f13ceaef44f5.png'),
-	(2, 'Captura de tela 2023-04-03 144854.png', 'f758a3846b98b2c6eb8bb6823bc6d088.png'),
-	(3, 'foto.png', 'ffd0833f46dabeff5f110a7d2e36753c.png'),
-	(4, '1234.png', '1234.png');
-/*!40000 ALTER TABLE `imagens` ENABLE KEYS */;
+	(25, 'ye.png', '3b410035b96de019de2fdefc2f025954.png');
 
 -- Copiando estrutura para tabela uploadarquivos.posts
 CREATE TABLE IF NOT EXISTS `posts` (
   `blog_codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `blog_bloginfo_codigo` int(11) NOT NULL DEFAULT '0',
-  `blog_blogimgs_codigo` int(11) NOT NULL DEFAULT '0',
-  `blog_usuario_codigo` int(11) NOT NULL DEFAULT '0',
+  `blog_bloginfo_codigo` int(11) NOT NULL DEFAULT 0,
+  `blog_blogimgs_codigo` int(11) NOT NULL DEFAULT 0,
+  `blog_usuario_codigo` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`blog_codigo`),
   KEY `FK_blog_bloginfo` (`blog_bloginfo_codigo`),
   KEY `FK_blog_blogimgs` (`blog_blogimgs_codigo`),
@@ -63,14 +59,12 @@ CREATE TABLE IF NOT EXISTS `posts` (
   CONSTRAINT `FK_blog_blogimgs` FOREIGN KEY (`blog_blogimgs_codigo`) REFERENCES `imagens` (`id_imagem`),
   CONSTRAINT `FK_blog_bloginfo` FOREIGN KEY (`blog_bloginfo_codigo`) REFERENCES `bloginfo` (`bloginfo_codigo`),
   CONSTRAINT `FK_blog_usuario` FOREIGN KEY (`blog_usuario_codigo`) REFERENCES `usuario` (`usuario_codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Copiando dados para a tabela uploadarquivos.posts: ~1 rows (aproximadamente)
 DELETE FROM `posts`;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
 INSERT INTO `posts` (`blog_codigo`, `blog_bloginfo_codigo`, `blog_blogimgs_codigo`, `blog_usuario_codigo`) VALUES
-	(1, 2, 4, 1);
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+	(26, 30, 25, 2);
 
 -- Copiando estrutura para tabela uploadarquivos.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -79,15 +73,17 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `usuario_email` varchar(250) NOT NULL DEFAULT '0',
   `usuario_senha` varchar(250) NOT NULL DEFAULT '0',
   PRIMARY KEY (`usuario_codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Copiando dados para a tabela uploadarquivos.usuario: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela uploadarquivos.usuario: ~2 rows (aproximadamente)
 DELETE FROM `usuario`;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
 INSERT INTO `usuario` (`usuario_codigo`, `usuario_nome`, `usuario_email`, `usuario_senha`) VALUES
-	(1, 'igor', 'igor@gmail', '123');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+	(1, 'igor', 'igor@gmail', '123'),
+	(2, 'arthur', 'arthur@gmail', '123'),
+	(3, 'Clara', 'c@c', '123');
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- models/conexao.php
+++ models/conexao.php
@@ -3,15 +3,11 @@ $servidor = "127.0.0.1";
 $usuario = "root";
 $senha = "";
 $banco = "uploadarquivos";
-
 $conexao =  mysqli_connect($servidor, $usuario, $senha);
             mysqli_select_db($conexao, $banco);
             mysqli_set_charset($conexao, "UTF8");
-
 /*  if ($conexao) {
      echo "conectado";
  } else {
      echo "erro"; } */
-
-
-?><!--  -->
+?>
